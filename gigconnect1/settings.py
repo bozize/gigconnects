f9d090749aba62d4d33841d8a3e86f50b4bae927
boziz <bozize@gmail.com>
2024-09-23 15:59:21 +0300

    updated settings.py

--- gigconnect1/settings.py
+++ gigconnect1/settings.py
@@ -52,7 +52,7 @@ CORS_ALLOW_HEADERS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://gigconnect1-ckln0ezge-bozizes-projects.vercel.app",
+    "https://gigconnect1-1enuv0l3o-bozizes-projects.vercel.app/",
 ]
 
 
