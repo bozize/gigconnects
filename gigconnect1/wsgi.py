da4a5f06abb6bcbb9d9419080f8b5dc1da13416e
boziz <bozize@gmail.com>
2024-09-23 03:17:26 +0300

    update

--- gigconnect1/wsgi.py
+++ gigconnect1/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gigconnect1.settings")
 
 application = get_wsgi_application()
+
+# Automatically create superuser if it doesn't exist
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username=os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')).exists():
+    User.objects.create_superuser(
+        username=os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin'),
+        email=os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com'),
+        password=os.getenv('DJANGO_SUPERUSER_PASSWORD', '0711169847')
+    )
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
